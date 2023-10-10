9c8f0860380be940db6c6690c6135a4a97120c94
ak47 <1107624590@qq.com>
2023-10-10 20:54:54 +0800

    用栈实现递归操作

--- code/main.go
+++ code/main.go
@@ -71,7 +71,7 @@ func main4() {
 	fmt.Println(err)
 }
 
-func main() {
+func main5() {
 	mystack := ArrayList.NewArrayListStack()
 	mystack.Push(1)
 	mystack.Push(2)
@@ -86,4 +86,40 @@ func main() {
 		item, _ := it.Next()
 		fmt.Println(item)
 	}
+}
+
+// 用栈模拟递归实现1到5的累加
+func main6() {
+	mystack := StackArray.NewStack()
+	mystack.Push(5)
+	last := 0
+	for !mystack.IsEmpty() {
+		data := mystack.Pop()
+		if data == 0 {
+			break
+		}
+		last += data.(int)
+		mystack.Push(data.(int) - 1)
+
+	}
+
+	fmt.Println(last)
+}
+
+// 用栈模递归实现斐波那契数列
+func main() {
+	mystack := StackArray.NewStack()
+	mystack.Push(7)
+	last := 0
+	for !mystack.IsEmpty() {
+		data := mystack.Pop()
+		if data == 1 || data == 2 {
+			last += 1
+		} else {
+			mystack.Push(data.(int) - 1)
+			mystack.Push(data.(int) - 2)
+		}
+	}
+
+	fmt.Println(last)
 }
