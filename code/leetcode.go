4c06af46f0be2b2cae7b6398a9aaa68ae15c52af
junbo <1107624590@qq.com>
2023-11-08 17:20:18 +0800

    返回数组中个数最多的元素

--- code/leetcode.go
+++ code/leetcode.go
@@ -48,32 +48,6 @@ func mergeArr(arr1 []int, arr2 []int) []int {
 	return finalArr
 }
 
-func main() {
-	arr1 := []int{1, 3, 4, 5, 7, 9, 23}
-	arr2 := []int{2, 3, 5, 7, 8, 10, 33, 44, 55, 66}
-
-	finalArr := mergeArr(arr1, arr2)
-
-	fmt.Println(finalArr)
-
-	arr3 := []int{1, 1, 1, 2, 2, 4, 6, 33, 33, 33}
-
-	//lenArr3 := len(arr3)
-	//len := removeElement(arr3, 1)
-	//arr3 = arr3[:lenArr3-(lenArr3-len)]
-	//
-	//fmt.Println(len)
-	//fmt.Println(arr3)
-
-	lenArr3 := len(arr3)
-	len := removeDuplicates2(arr3)
-	arr3 = arr3[:lenArr3-(lenArr3-len)]
-
-	fmt.Println(len)
-	fmt.Println(arr3)
-
-}
-
 // 删除有序数组中的重复项
 func removeDuplicates(arr []int) int {
 	len := len(arr)
@@ -114,4 +88,59 @@ func removeDuplicates2(arr []int) int {
 	}
 
 	return slow
+}
+
+func countNums(nums []int) map[int]int {
+	counts := make(map[int]int)
+	for _, num := range nums {
+		counts[num]++
+	}
+	return counts
+}
+
+// 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+func majorityElement(nums []int) int {
+	counts := countNums(nums)
+
+	majorityEntry := counts[0]
+	for _, entry := range counts {
+		if entry > majorityEntry {
+			majorityEntry = entry
+		}
+	}
+	for key, value := range counts {
+		if value == majorityEntry {
+			return key
+		}
+	}
+	return -1
+}
+
+func main() {
+	arr1 := []int{1, 3, 4, 5, 7, 9, 23}
+	arr2 := []int{2, 3, 5, 7, 8, 10, 33, 44, 55, 66}
+
+	finalArr := mergeArr(arr1, arr2)
+
+	fmt.Println(finalArr)
+
+	arr3 := []int{1, 1, 1, 2, 2, 4, 6, 33, 33, 33, 1}
+
+	//lenArr3 := len(arr3)
+	//len := removeElement(arr3, 1)
+	//arr3 = arr3[:lenArr3-(lenArr3-len)]
+	//
+	//fmt.Println(len)
+	//fmt.Println(arr3)
+
+	//lenArr3 := len(arr3)
+	//len := removeDuplicates2(arr3)
+	//arr3 = arr3[:lenArr3-(lenArr3-len)]
+	//
+	//fmt.Println(len)
+	//fmt.Println(arr3)
+
+	fmt.Println(majorityElement(arr3))
+
 }
